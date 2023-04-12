5ae7b4ddb0f53cc245d5289dec250e5ff57db7ae
Matthew R. Becker <beckermr@users.noreply.github.com>
2023-04-12 13:34:52 -0500

    ENH version update dispatch command (#29)
    * WIP version update dispatch command
    
    * BUG need old test file
    
    * TST allow empty commit
    
    * BUG fix version update test
    
    * TST try again
    
    * ENH more bugsz
    
    * ENH adjust tests one more time
    
    * TST try tests again

---
+++ tests/run_live_version_update_test.py
@@ -0,0 +1,234 @@
+"""
+This script will run a live integration test of the version update
+action. To use it do the following
+
+1. Make sure you have a valid github token in your environment called
+   "GH_TOKEN".
+2. Make surre you have pushed the new version of the action to the `dev`
+   docker image tag.
+
+   You can run
+
+      docker build -t condaforge/webservices-dispatch-action:dev .
+      docker push condaforge/webservices-dispatch-action:dev
+
+   or pass `--build-and-push` when running the test script.
+
+Then you can execute this script and it will report the results.
+
+## setup
+
+ - The script uses a PR on the `conda-forge/cf-autotick-bot-test-package-feedstock`.
+ - The head ref for this PR is on a fork of that feedstock in the regro
+   organization.
+ - It works by pushing a change to the PR branch that would cause a rerender
+   to happen.
+ - Then we trigger the rerender and check that it happened.
+
+"""
+import os
+import json
+import time
+import tempfile
+import requests
+import contextlib
+import subprocess
+import argparse
+
+BRANCH = "version-update-live-test"
+PR_NUM = 483
+
+
+@contextlib.contextmanager
+def pushd(new_dir):
+    previous_dir = os.getcwd()
+    os.chdir(new_dir)
+    try:
+        yield
+    finally:
+        os.chdir(previous_dir)
+
+
+def _change_version(new_version="0.13", branch="main"):
+    print("changing the version to an old one...", flush=True)
+    subprocess.run(f"git checkout {branch}", shell=True, check=True)
+
+    new_lines = []
+    with open("recipe/meta.yaml", "r") as fp:
+        for line in fp.readlines():
+            if line.startswith('{% set version ='):
+                new_lines.append('{%% set version = "%s" %%}\n' % new_version)
+            elif line.startswith("  sha256: "):
+                new_lines.append("  sha256: blah\n")
+            else:
+                new_lines.append(line)
+    with open("recipe/meta.yaml", "w") as fp:
+        fp.write("".join(new_lines))
+
+    print("staging file..", flush=True)
+    subprocess.run("git add recipe/meta.yaml", shell=True, check=True)
+    subprocess.run(
+        "git commit "
+        "--allow-empty "
+        "-m "
+        "'[ci skip] moved version to older 0.13'",
+        shell=True, check=True
+    )
+
+    print("push to origin...", flush=True)
+    subprocess.run("git push", shell=True, check=True)
+
+
+def _merge_main_to_branch():
+    print("merging main into branch...", flush=True)
+    subprocess.run("git checkout main", shell=True, check=True)
+    subprocess.run("git pull", shell=True, check=True)
+    subprocess.run(f"git checkout {BRANCH}", shell=True, check=True)
+    subprocess.run("git pull", shell=True, check=True)
+    subprocess.run("git merge --no-edit main", shell=True, check=True)
+    subprocess.run("git push", shell=True, check=True)
+
+
+def _run_test():
+    print('sending repo dispatch event to update the version...', flush=True)
+    headers = {
+        "authorization": "Bearer %s" % os.environ['GH_TOKEN'],
+        'content-type': 'application/json',
+    }
+    r = requests.post(
+        ("https://api.github.com/repos/conda-forge/"
+         "cf-autotick-bot-test-package-feedstock/dispatches"),
+        data=json.dumps(
+            {
+                "event_type": "version_update",
+                "client_payload": {"pr": PR_NUM},
+            }
+        ),
+        headers=headers,
+    )
+    print('    dispatch event status code:', r.status_code, flush=True)
+    assert r.status_code == 204
+
+    print('sleeping for a few minutes to let the version update happen...', flush=True)
+    tot = 0
+    while tot < 180:
+        time.sleep(10)
+        tot += 10
+        print("    slept %s seconds out of 180" % tot, flush=True)
+
+    print('checking repo for the version update...', flush=True)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        with pushd(tmpdir):
+            print("cloning...", flush=True)
+            subprocess.run(
+                "git clone "
+                "https://github.com/conda-forge/"
+                "cf-autotick-bot-test-package-feedstock.git",
+                shell=True,
+                check=True,
+            )
+
+            with pushd("cf-autotick-bot-test-package-feedstock"):
+                print("checkout branch...", flush=True)
+                subprocess.run(
+                    f"git checkout {BRANCH}",
+                    shell=True,
+                    check=True,
+                )
+
+                print("checking the git history", flush=True)
+                c = subprocess.run(
+                    ["git", "log", "--pretty=oneline", "-n", "1"],
+                    capture_output=True,
+                    check=True,
+                )
+                output = c.stdout.decode('utf-8')
+                print("    last commit:", output.strip(), flush=True)
+                assert "MNT:" in output or "ENH " in output
+
+    print('tests passed!', flush=True)
+
+
+def _change_action_branch(branch):
+    print("moving repo to %s action" % branch, flush=True)
+    subprocess.run("git checkout main", shell=True, check=True)
+
+    data = (
+        branch,
+        "rerendering_github_token: ${{ secrets.RERENDERING_GITHUB_TOKEN }}",
+    )
+
+    with open(".github/workflows/webservices.yml", "w") as fp:
+        fp.write("""\
+on: repository_dispatch
+
+jobs:
+  webservices:
+    runs-on: ubuntu-latest
+    name: webservices
+    steps:
+      - name: webservices
+        id: webservices
+        uses: conda-forge/webservices-dispatch-action@%s
+        with:
+          github_token: ${{ secrets.GITHUB_TOKEN }}
+          %s
+""" % data)
+
+    print("commiting...", flush=True)
+    subprocess.run("git add .github/workflows/webservices.yml", shell=True, check=True)
+    subprocess.run(
+        "git commit "
+        "--allow-empty "
+        "-m "
+        "'[ci skip] move rerender action to branch %s'" % branch,
+        shell=True, check=True
+    )
+
+    print("push to origin...", flush=True)
+    subprocess.run("git push", shell=True, check=True)
+
+
+parser = argparse.ArgumentParser(
+    description="Run a live test of the rerendering code",
+)
+parser.add_argument(
+    "--build-and-push",
+    help="build and push the docker image to the dev tag before running the tests",
+    action="store_true",
+)
+args = parser.parse_args()
+
+if args.build_and_push:
+    subprocess.run(
+        "docker build -t condaforge/webservices-dispatch-action:dev .",
+        shell=True,
+    )
+    subprocess.run(
+        "docker push condaforge/webservices-dispatch-action:dev",
+        shell=True,
+    )
+
+
+print('making an edit to the head ref...', flush=True)
+with tempfile.TemporaryDirectory() as tmpdir:
+    with pushd(tmpdir):
+        print("cloning...", flush=True)
+        subprocess.run(
+            "git clone "
+            "https://x-access-token:${GH_TOKEN}@github.com/conda-forge/"
+            "cf-autotick-bot-test-package-feedstock.git",
+            shell=True,
+            check=True,
+        )
+
+        with pushd("cf-autotick-bot-test-package-feedstock"):
+            try:
+                _change_action_branch("dev")
+                _change_version(new_version="0.13", branch="main")
+                _merge_main_to_branch()
+                _run_test()
+            finally:
+                _change_action_branch("main")
+                _change_version(new_version="0.14", branch="main")
+                _merge_main_to_branch()
--- webservices_dispatch_action/__main__.py
+++ webservices_dispatch_action/__main__.py
@@ -11,8 +11,13 @@ from webservices_dispatch_action.api_sessions import (
 )
 from webservices_dispatch_action.rerendering import (
     rerender,
-    comment_and_push_per_changed,
+    rerender_comment_and_push_per_changed,
 )
+from webservices_dispatch_action.version_updater import (
+    update_version,
+    version_comment_and_push_per_changed
+)
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -29,7 +34,7 @@ def main():
     event_name = os.environ['GITHUB_EVENT_NAME'].lower()
 
     LOGGER.info('github event: %s', event_name)
-    LOGGER.info('github event data:\n%s', pprint.pformat(event_data))
+    LOGGER.info('github event data:\n%s\n', pprint.pformat(event_data))
 
     if event_name in ['repository_dispatch']:
         if event_data['action'] == 'rerender':
@@ -66,7 +71,7 @@ def main():
                 changed, rerender_error = rerender(git_repo, can_change_workflows)
 
                 # comment
-                push_error = comment_and_push_per_changed(
+                push_error = rerender_comment_and_push_per_changed(
                     changed=changed,
                     rerender_error=rerender_error,
                     git_repo=git_repo,
@@ -84,6 +89,82 @@ def main():
                             rerender_error,
                         ),
                     )
+        elif event_data['action'] == 'version_update':
+            pr_num = int(event_data['client_payload']['pr'])
+            repo_name = event_data['repository']['full_name']
+
+            gh_repo = gh.get_repo(repo_name)
+            pr = gh_repo.get_pull(pr_num)
+
+            if pr.state == 'closed':
+                raise ValueError("Closed PRs cannot be rerendered!")
+
+            with tempfile.TemporaryDirectory() as tmpdir:
+                # clone the head repo
+                pr_branch = pr.head.ref
+                pr_owner = pr.head.repo.owner.login
+                pr_repo = pr.head.repo.name
+                repo_url = "https://github.com/%s/%s.git" % (
+                    pr_owner,
+                    pr_repo,
+                )
+                feedstock_dir = os.path.join(
+                    tmpdir,
+                    pr_repo,
+                )
+                git_repo = Repo.clone_from(
+                    repo_url,
+                    feedstock_dir,
+                    branch=pr_branch,
+                )
+
+                _, _, can_change_workflows = get_actor_token()
+
+                # update version
+                version_changed, version_error = update_version(git_repo, repo_name)
+                version_push_error = version_comment_and_push_per_changed(
+                    changed=version_changed,
+                    version_error=version_error,
+                    git_repo=git_repo,
+                    pull=pr,
+                    pr_branch=pr_branch,
+                    pr_owner=pr_owner,
+                    pr_repo=pr_repo,
+                    repo_name=repo_name,
+                )
+
+                if version_error or version_push_error:
+                    raise RuntimeError(
+                        "Updating version failed! error in "
+                        "push|version update: %s|%s" % (
+                            version_push_error,
+                            version_error,
+                        ),
+                    )
+
+                if version_changed:
+                    # rerender
+                    rerender_changed, rerender_error = rerender(
+                        git_repo, can_change_workflows
+                    )
+                    rerender_push_error = rerender_comment_and_push_per_changed(
+                        changed=rerender_changed,
+                        rerender_error=rerender_error,
+                        git_repo=git_repo,
+                        pull=pr,
+                        pr_branch=pr_branch,
+                        pr_owner=pr_owner,
+                        pr_repo=pr_repo,
+                        repo_name=repo_name,
+                    )
+
+                    if rerender_error or rerender_push_error:
+                        raise RuntimeError(
+                            "Rerendering failed! error in push|rerender: %s|%s" % (
+                                push_error,
+                                rerender_error,
+                            ),
+                        )
         else:
             raise ValueError(
                 'Dispatch action %s cannot be processed!' % event_data['action'])
